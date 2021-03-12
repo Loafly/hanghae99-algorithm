6e0b864c1b41cce57e3edd56358ceb27aeb77c6b
NamSeok_DT <skatjr30@naver.com>
2021-03-12 17:39:23 +0900

    1011.py

--- 2021-03-12/1011.py
+++ 2021-03-12/1011.py
@@ -1,2 +1,22 @@
+import math
 import sys
-length = st
+length = int(sys.stdin.readline())
+
+for i in range(length):
+    date_conut = 0
+    x, y = sys.stdin.readline().split()
+    x = int(x)
+    y = int(y)
+    distance = y - x
+    if distance <= 3:
+        print(distance)
+    else:        
+        square_root = int(math.sqrt(distance))
+        
+        if distance == square_root ** 2:
+            date_conut = 2 * square_root - 1
+        elif distance > square_root ** 2  and distance <= square_root ** 2 + square_root:
+            date_conut = 2 * square_root
+        elif distance > square_root **2 + square_root:
+            date_conut = 2 * square_root + 1    
+        print(date_conut)
