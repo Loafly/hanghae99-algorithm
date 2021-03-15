4d90587461f5965a510eeb52921ffdb8c9f25326
NamSeok_DT <skatjr30@naver.com>
2021-03-15 21:58:34 +0900

    2021-03-15

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 #변수를 입력받는 방법
-a,b = input().split()
+# a,b = input().split()
+#
+# #문자를 숫자로
+# a = int(a)
+# b = int(b)
+#
+# #풀이
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(int(a/b))
+# print(a%b)
 
-#문자를 숫자로
-a = int(a)
-b = int(b)
-
-#풀이
-print(a+b)
-print(a-b)
-print(a*b)
-print(int(a/b))
-print(a%b)
